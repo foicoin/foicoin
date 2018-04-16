c3e3e57822e2a0d962df8746ff8014ee56e66b62
foicoin <36694651+foicoin@users.noreply.github.com>
2018-04-16 16:51:12 +0900

    Update bootnodes.go

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -20,9 +20,9 @@ package params
 // the main Ethereum network.
 var MainnetBootnodes = []string{
 	//Linux server bootnode
-	"enode://2dfb4ada8694abd3bfbc60c91924e5e4a7ab00ab77fa916d04df7a1dccd081a1368138a85d363988b21bc315867c8a86859cb5a743eff609ddffb3d324a54cfd@52.231.195.25:30303",
+	"enode://87b1abcd01f7d806e4729032fed20dffe7dc009dd9c5acbbbfac149b1348b9cf0acce4d0d093d06b2400bc494db4a387bec7d450e6556c8495ac0dd03bf5a020@52.231.195.25:30303",
 	//windows server bootnode
-	"enode://f85d61e8c6ea3d7695a0fdc83309efedd2e345649cc8e3097958fd97dbba2bd0b56f49aeed43a1a0ad3f12221f51e8f86dd6ce222ee8b333713d86eecdfe5d7f@104.42.143.8:30303",
+	"enode://77cfbe66f669df2d40ec1405ee027e6fb726951dff2bc8bd44cd0ca00ec9b9f652806b2aa88688eb4411cf054930106f82ab2377d47d3a62d5877e3c7a1dc690@104.42.143.8:30303",
 }
 
 // TestnetBootnodes are the enode URLs of the P2P bootstrap nodes running on the
